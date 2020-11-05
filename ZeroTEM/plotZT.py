30e829abd0dcd7dab8c7097ee90ca37a64d9caea
Trevor Irons <Trevor.Irons@lemmasoftware.org>
2020-11-05 14:06:51 -0700

    DOX mv move

--- ZeroTEM/plotZT.py
+++ ZeroTEM/plotZT.py
@@ -650,7 +650,7 @@ class ZeroTEMSounding( TEMSounding ):
         """ Calls AMIRA Beowulf inversion. 
         """
         subprocess.call("./Beowulf")
-        subprocess.call(["mv", "Beowulf.mv1", sdir+".mv1"])
+        subprocess.call(["move", "Beowulf.mv1", sdir+".mv1"])
 
     def readMV1(self, filename):
         with open(filename) as mv1:
